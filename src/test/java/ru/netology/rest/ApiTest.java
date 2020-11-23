ff75b711ac8d01d5efa811b9d454aa4d7bb1945c
melomanos <philippov_mihail@mail.ru>
2020-11-23 16:17:43 +0300

    Initial commit and build failed

---
+++ src/test/java/ru/netology/rest/ApiTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ApiTest {
+
+    @Test
+    void shouldJsonPath() {
+        // Given - When - Then
+        // Предусловия
+        given()
+            .baseUri("https://postman-echo.com")
+            .body("Hello world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+        .when()
+            .post("/post")
+        // Проверки
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("some data"))
+        ;
+    }
+}
