286def742de36001415c0e448b30f6994e1e2640
melomanos <philippov_mihail@mail.ru>
2020-11-23 16:23:03 +0300

    Build must passed

--- src/test/java/ru/netology/rest/ApiTest.java
+++ src/test/java/ru/netology/rest/ApiTest.java
@@ -20,7 +20,7 @@ public class ApiTest {
         // Проверки
         .then()
             .statusCode(200)
-            .body("data", equalTo("some data"))
+            .body("data", equalTo("Hello world!"))
         ;
     }
 }
